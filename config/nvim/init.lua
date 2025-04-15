3e64e6213b2e343928b7c5df93457ef3ba7d3533
LuisMendez-ACC <luis.mendez2@g.austincc.edu>
2025-04-15 17:30:21 -0500

    Additional configuration for dashboard

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -239,7 +239,9 @@ vim.opt.rtp:prepend(lazypath)
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+  'folke/lazy.nvim', -- plugin manager
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  'nvimdev/dashboard-nvim', -- A dashboard for neovim
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
