442aea9dcd63e5a6a211267d2b0b75d1f44cb583
Mendez, Luis A SGT SOFTWARE FACTORY (USA) <luis.a.mendez48.mil@army.mil>
2025-04-21 07:56:17 -0500

    Fixed error popping up on nvim init about No spec for module 'plugins' and tweaked dashboard.lua configs. TODO: Get Dashboard to render on init.

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -228,7 +228,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 -- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+require 'config.lazy'
 --endregion Plugin-Manager
 
 -- [[ Configure and install plugins ]]
@@ -243,82 +243,82 @@ require("config.lazy")
 
 -- NOTE: Here is where you install your plugins.
 
-  -- NOTE: Plugins can also be added by using a table,
-  -- with the first argument being the link and the following
-  -- keys can be used to configure plugin behavior/loading/etc.
+-- NOTE: Plugins can also be added by using a table,
+-- with the first argument being the link and the following
+-- keys can be used to configure plugin behavior/loading/etc.
 
-  -- Use `opts = {}` to automatically pass options to a plugin's `setup()` function, forcing the plugin to be loaded.
+-- Use `opts = {}` to automatically pass options to a plugin's `setup()` function, forcing the plugin to be loaded.
 
-  -- Alternatively, use `config = function() ... end` for full control over the configuration.
-  -- If you prefer to call `setup` explicitly, use:
-  --    {
-  --        'lewis6991/gitsigns.nvim',
-  --        config = function()
-  --            require('gitsigns').setup({
-  --                -- Your gitsigns configuration here
-  --            })
-  --        end,
-  --    }
-  -- Here is a more advanced example where we pass configuration
-  -- options to `gitsigns.nvim`.
+-- Alternatively, use `config = function() ... end` for full control over the configuration.
+-- If you prefer to call `setup` explicitly, use:
+--    {
+--        'lewis6991/gitsigns.nvim',
+--        config = function()
+--            require('gitsigns').setup({
+--                -- Your gitsigns configuration here
+--            })
+--        end,
+--    }
+-- Here is a more advanced example where we pass configuration
+-- options to `gitsigns.nvim`.
 
-  -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
+-- NOTE: Plugins can also be configured to run Lua code when they are loaded.
 
-  -- This is often very useful to both group configuration, as well as handle
-  -- lazy loading plugins that don't need to be loaded immediately at startup.
+-- This is often very useful to both group configuration, as well as handle
+-- lazy loading plugins that don't need to be loaded immediately at startup.
 
-  -- For example, in the following configuration, we use:
-  --  event = 'VimEnter'
+-- For example, in the following configuration, we use:
+--  event = 'VimEnter'
 
-  -- which loads which-key before all the UI elements are loaded. Events can be
-  -- normal autocommands events (`:help autocmd-events`).
+-- which loads which-key before all the UI elements are loaded. Events can be
+-- normal autocommands events (`:help autocmd-events`).
 
-  -- Then, because we use the `opts` key (recommended), the configuration runs
-  -- after the plugin has been loaded as `require(MODULE).setup(opts)`.
+-- Then, because we use the `opts` key (recommended), the configuration runs
+-- after the plugin has been loaded as `require(MODULE).setup(opts)`.
 
-  -- NOTE: Plugins can specify dependencies.
+-- NOTE: Plugins can specify dependencies.
 
-  -- The dependencies are proper plugin specifications as well - anything
-  -- you do for a plugin at the top level, you can do for a dependency.
+-- The dependencies are proper plugin specifications as well - anything
+-- you do for a plugin at the top level, you can do for a dependency.
 
-  -- Use the `dependencies` key to specify the dependencies of a particular plugin
+-- Use the `dependencies` key to specify the dependencies of a particular plugin
 
-  -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
-  -- init.lua. If you want these files, they are in the repository, so you can just download them and
-  -- place them in the correct locations.
+-- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
+-- init.lua. If you want these files, they are in the repository, so you can just download them and
+-- place them in the correct locations.
 
-  --region Kickstart-Plugins
-  -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
-  --
-  --  Here are some example plugins that I've included in the Kickstart repository.
-  --  Uncomment any of the lines below to enable them (you will need to restart nvim).
-  --
-  -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
-  --endregion Kickstart-Plugins
+--region Kickstart-Plugins
+-- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
 
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    This is the easiest way to modularize your config.
+--  Here are some example plugins that I've included in the Kickstart repository.
+--  Uncomment any of the lines below to enable them (you will need to restart nvim).
 
-  --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' }
+-- require 'kickstart.plugins.debug',
+-- require 'kickstart.plugins.indent_line',
+-- require 'kickstart.plugins.lint',
+-- require 'kickstart.plugins.autopairs',
+-- require 'kickstart.plugins.neo-tree',
+-- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+--endregion Kickstart-Plugins
 
-  -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
-  -- Or use telescope!
-  -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
-  -- you can continue same window with `<space>sr` which resumes last telescope search
+-- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
+--    This is the easiest way to modularize your config.
+
+--  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
+-- { import = 'custom.plugins' }
+
+-- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
+-- Or use telescope!
+-- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
+-- you can continue same window with `<space>sr` which resumes last telescope search
 
 -- Disable the concealing in some file formats
 -- The default conceallevel is 3 in LazyVim
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "json", "jsonc", "markdown" },
-	callback = function()
-		vim.wo.conceallevel = 0
-	end,
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'json', 'jsonc', 'markdown' },
+  callback = function()
+    vim.wo.conceallevel = 0
+  end,
 })
 
 -- require("luarocks-nvim").setup()
@@ -376,3 +376,4 @@ vim.api.nvim_create_autocmd("FileType", {
 --vim.cmd('silent! colorscheme seoul256')
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
